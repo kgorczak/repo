c7ff663816c3c489d06442f7d7212fb9d6709832
kgorczak <krzysztofgorczak@gmail.com>
2013-01-21 10:00:32 -0800

    Create delta.sh

---
+++ delta.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+    echo "Podaj A: "
+    read A
+    echo "Podaj B: "
+    read B
+    echo "Podaj C: "
+    read C
+    DELTA="$((B*B-4*A*C))"
+    if [ $DELTA -ge 0 ]; then
+    PIERW=`echo "scale=5 ; sqrt($DELTA)" | bc`
+    fi
+    CZYZERO=`echo "$DELTA==0" | bc`
+    CZYWIEKSZE=`echo "$DELTA>0" |bc`
+    echo "Delta wynosi: $DELTA"
+     echo "Pierwiastek z Delty wynosi: $PIERW"
+
+        if [ "$CZYZERO" = 1 ]; then
+           P0=`echo "scale=5 ;(-1*$B)/(2*$A)" | bc`
+            echo "Rownanie posiada jeden pierwiastek: $P0"
+        elif [ "$CZYWIEKSZE" = 1 ]; then
+            P1=`echo "scale=5 ;(-1*$B-$PIERW)/(2*$A)" | bc`
+            P2=`echo "scale=5 ;(-1*$B+$PIERW)/(2*$A)" | bc`
+            echo "Rownanie posiada dwa pierwiastki: $P1  i  $P2"
+        else
+            echo "To rownanie nie posiada pierwiastkow"
+        fi
+
+
+
