09219670636cf2e2c8731fa5ed3663e62f6efdbe
Krzysztof Górczak <kgorczak@p109-3.labpk.inf.ug.edu.pl>
2013-01-22 08:46:44 +0100

    ang

---
+++ silnia.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#Krzysztof Gorczak
+
+#Skrypt obliczajacy silnie dla podanego argumentu
+#Wywolanie: skrypt.sh LICZBA lub skrypt.sh, np. silnia.sh 5 lub silnia.sh 
+#W momencie wywołania bez argumentu, skrypt poprosi nas o jego podanie
+#Enjoy!
+
+#Funkcja liczaca silnie
+function factorial(){
+	#Zmienne pomocnicze
+	result=1
+	iter=1;
+	
+	#Liczenie silni
+	while [ $number -ge $iter ]; do
+		result=$(($result * $iter))
+		iter=$(($iter + 1 ))
+	done
+
+	#Drukowanie wyniku
+	echo "Factorial is: $result"
+exit 0
+}
+
+#Sprawdzam czy liczba argumentow jest wieksza niz 1, jezeli tak to koniec
+if [ $# -gt 1 ]; then
+        echo "Error, too many arguments!"
+        exit 1
+fi
+
+#Sprawdzam czy nie podano zadnych argumentow
+if [ $# -eq 0 ]; then
+        echo "No agruments, enter the number in the range: 0-20"
+        read hand
+    #Sprawdzam czy podany argument w takcie dzialania skryptu jest liczba i jest z zakresu (0 - 20)
+        if [ $hand -eq $hand 2> /dev/null ] && [ $hand -ge 0 2> /dev/null ] && [ $hand -le 20 2> /dev/null ]; then
+                number=$hand
+        echo "Start counting factorial: $number!"
+		#Warunki spełnione, licz silnie
+		factorial
+        else
+                echo "$hand it's wrong argument, must be number in range: 0-20"
+                exit 1
+    fi
+else
+    #Sprawdzam czy podany argument z wiersza polecen jest liczba i i jest z zakresu (0 - 20)
+    if [ $1 -eq $1 2> /dev/null ] && [ $1 -ge 0 2> /dev/null ] && [ $1 -le 20 2> /dev/null ]; then
+        number=$1
+        echo "Start counting factorial: $number!"
+		#Warunki spełnione, licz silnie
+		factorial
+        else
+                echo "$1 it's wrong argument, must be number in range: 0-20"
+        exit 1
+    fi
+fi
